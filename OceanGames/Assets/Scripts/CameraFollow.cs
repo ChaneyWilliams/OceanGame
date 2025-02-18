173485a61ca4a17994d456b39aadfb28e99bbccb
ChaneyWilliams <chaneyw2004@gmail.com>
2025-02-17 18:18:09 -0600

    Camera Follows

---
+++ OceanGames/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform player;
+    Vector3 camerapos;
+    float yValue = 0;
+
+    public void Awake()
+    {
+        camerapos = this.transform.position;
+    }
+
+    private void Update()
+    {
+        if (player.position.y > camerapos.y + 5)
+        {
+            yValue = player.position.y - 5f;
+        }
+        else if (player.position.y < camerapos.y - 5)
+        {
+            yValue = player.position.y + 5f;
+        }
+        this.transform.position = new Vector3(camerapos.x + player.position.x, yValue, -10);
+    }
+}
